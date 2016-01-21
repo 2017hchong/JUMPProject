16ac76e910a4d40da0fb23b9b930d2cca900aaae
Hayun Chong <hayunchong@Hayuns-MBP.home>
2016-01-21 10:32:52 -0500

    Initial commit

---
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/MainScreen.java
@@ -0,0 +1,8 @@
+package edu.cmu.pocketsphinx.demo;
+
+/**
+ * Created by hayunchong on 1/21/16.
+ */
+public class MainScreen {
+
+}
---
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
@@ -0,0 +1,286 @@
+
+
+package edu.cmu.pocketsphinx.demo;
+
+import static android.widget.Toast.makeText;
+import static edu.cmu.pocketsphinx.SpeechRecognizerSetup.defaultSetup;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.SlidingDrawer;
+import android.widget.TextView;
+import android.widget.Toast;
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+
+public class PocketSphinxActivity extends Activity implements
+        RecognitionListener {
+
+    Button slideButton;
+    SlidingDrawer slidingDrawer;
+
+
+    /* Named searches allow to quickly reconfigure the decoder */
+    private static final String KWS_SEARCH = "wakeup";
+
+    //private static final String FORECAST_SEARCH = "forecast";
+    //private static final String DIGITS_SEARCH = "digits";
+    //private static final String PHONE_SEARCH = "phones";
+    //private static final String MENU_SEARCH = "menu";
+    private static final String COMMANDS_SEARCH = "commands";
+
+    /* Keyword we are looking for to activate menu */
+    private static final String KEYPHRASE = "take picture";
+
+    private SpeechRecognizer recognizer;
+    private HashMap<String, Integer> captions;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+
+
+
+        // Prepare the data for UI
+        captions = new HashMap<String, Integer>();
+        captions.put(KWS_SEARCH, R.string.kws_caption);
+        captions.put(COMMANDS_SEARCH, R.string.commands_caption);
+        setContentView(R.layout.main);
+        ((TextView) findViewById(R.id.caption_text))
+                .setText("Preparing the recognizer");
+
+
+        slideButton = (Button) findViewById(R.id.slideButton);
+        slidingDrawer = (SlidingDrawer) findViewById(R.id.SlidingDrawer);
+
+        slidingDrawer.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener() {
+            @Override
+            public void onDrawerOpened() {
+                slideButton.setText("Close Help");
+                switchSearch(KWS_SEARCH);
+            }
+        });
+        slidingDrawer.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener() {
+            @Override
+            public void onDrawerClosed() {
+                slideButton.setText("Open Help");
+            }
+        });
+
+        // Recognizer initialization is a time-consuming and it involves IO,
+        // so we execute it in async task
+
+        new AsyncTask<Void, Void, Exception>() {
+            @Override
+            protected Exception doInBackground(Void... params) {
+                try {
+                    Assets assets = new Assets(PocketSphinxActivity.this);
+                    File assetDir = assets.syncAssets();
+                    setupRecognizer(assetDir);
+                } catch (IOException e) {
+                    return e;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Exception result) {
+                if (result != null) {
+                    ((TextView) findViewById(R.id.caption_text))
+                            .setText("Failed to init recognizer " + result);
+                } else {
+                    switchSearch(KWS_SEARCH);
+                }
+            }
+        }.execute();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        recognizer.cancel();
+        recognizer.shutdown();
+    }
+    
+    /**
+     * In partial result we get quick updates about current hypothesis. In
+     * keyword spotting mode we can react here, in other modes we need to wait
+     * for final result in onResult.
+     */
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+        if (hypothesis == null)
+    	    return;
+
+        String text = hypothesis.getHypstr();
+        if (text.equals(KEYPHRASE))
+            switchSearch(COMMANDS_SEARCH);
+        else
+            ((TextView) findViewById(R.id.result_text)).setText(text);
+    }
+
+    //Translate word commands to degrees
+    public void takePic(String text)
+    {
+        int degree = commandToDegree(text);
+        rotateCamera(degree); //send signal to rotate camera and take picture
+    }
+    private int commandToDegree(String text)
+    {
+        int degree = -1;
+        switch(text)
+        {
+            case "right side":
+                degree = 90;
+                break;
+            case "left side":
+                degree = 270;
+                break;
+            case "front side":
+                degree = 0;
+                break;
+            case "back side":
+                degree = 180;
+                break;
+            case "one o clock":
+                degree = 30;
+                break;
+            case "two o clock":
+                degree = 60;
+                break;
+            case "three o clock":
+                degree = 90;
+                break;
+            case "four o clock":
+                degree = 120;
+                break;
+            case "five o clock":
+                degree = 150;
+                break;
+            case "six o clock":
+                degree = 180;
+                break;
+            case "seven o clock":
+                degree = 210;
+                break;
+            case "eight o clock":
+                degree = 240;
+                break;
+            case "nine o clock":
+                degree = 270;
+                break;
+            case "ten o clock":
+                degree = 300;
+                break;
+            case "eleven o clock":
+                degree = 330;
+                break;
+            case "twelve o clock":
+                degree = 0;
+                break;
+        }
+        return degree;
+    }
+
+    public void rotateCamera(int degree)
+    {
+        if(degree != -1)
+        {
+            //Rotate camera by degree
+            Log.i("TAKE PICTURE", "Rotate by this degree: "+degree);
+        }
+
+    }
+
+    /**
+     * This callback is called when we stop the recognizer.
+     */
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+        ((TextView) findViewById(R.id.result_text)).setText("");
+        if (hypothesis != null) {
+            String text = hypothesis.getHypstr();
+            makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            takePic(text);
+        }
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+    }
+
+    /**
+     * We stop recognizer here to get a final result
+     */
+    @Override
+    public void onEndOfSpeech() {
+        if (!recognizer.getSearchName().equals(KWS_SEARCH))
+            switchSearch(KWS_SEARCH);
+    }
+
+    private void switchSearch(String searchName) {
+        recognizer.stop();
+        
+        // If we are not spotting, start listening with timeout (10000 ms or 10 seconds).
+        if (searchName.equals(KWS_SEARCH))
+            recognizer.startListening(searchName);
+        else
+            recognizer.startListening(searchName, 10000);
+
+        String caption = getResources().getString(captions.get(searchName));
+        ((TextView) findViewById(R.id.caption_text)).setText(caption);
+    }
+
+    private void setupRecognizer(File assetsDir) throws IOException {
+        // The recognizer can be configured to perform multiple searches
+        // of different kind and switch between them
+        
+        recognizer = defaultSetup()
+                .setAcousticModel(new File(assetsDir, "en-us-ptm"))
+                //.setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
+                .setDictionary(new File(assetsDir, "commands.dict"))
+                // To disable logging of raw audio comment out this call (takes a lot of space on the device)
+                .setRawLogDir(assetsDir)
+                // Threshold to tune for keyphrase to balance between false alarms and misses
+                .setKeywordThreshold(1e-45f)
+                
+                // Use context-independent phonetic search, context-dependent is too slow for mobile
+                .setBoolean("-allphone_ci", true)
+                
+                .getRecognizer();
+        recognizer.addListener(this);
+
+        /** In your application you might not need to add all those searches.
+         * They are added here for demonstration. You can leave just one.
+         */
+
+        // Create keyword-activation search.
+        recognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
+
+        File commandsGrammar = new File(assetsDir, "commands.gram");
+        recognizer.addGrammarSearch(COMMANDS_SEARCH, commandsGrammar);
+
+
+    }
+
+    @Override
+    public void onError(Exception error) {
+        ((TextView) findViewById(R.id.caption_text)).setText(error.getMessage());
+    }
+
+    @Override
+    public void onTimeout() {
+        switchSearch(KWS_SEARCH);
+
+    }
+
+}
